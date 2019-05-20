f962487ceaf30a6020c38bdb4bba30e95dd516dc
anjali-jain0 <jainanjali043@gmail.com>
2019-05-20 23:35:43 +0530

    Fix key file path

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-	module.exports = require('key2');
+	module.exports = require('./key2');
 } else {
-	module.exports = require('key1');
+	module.exports = require('./key1');
 }
